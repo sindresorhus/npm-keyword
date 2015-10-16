7287b14d95c0ca194f7ae936110fe77898278d43
Kevin Martensson <kevinmartensson@gmail.com>
2015-10-16 19:17:27 +0200

    Use promises

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var got = require('got');
 var registryUrl = require('registry-url');
 
-function get(keyword, level, cb) {
+function get(keyword, level) {
 	if (typeof keyword !== 'string') {
 		throw new TypeError('Keyword must be a string');
 	}
@@ -15,52 +15,32 @@ function get(keyword, level, cb) {
 		'&endkey=[%22' + keyword + '%22,%7B%7D]' +
 		'&group_level=' + level;
 
-	got(url, function (err, data) {
-		if (err) {
-			cb(err);
-			return;
-		}
-
-		cb(null, JSON.parse(data).rows);
+	return got(url, {json: true}).then(function (res) {
+		return res.body.rows;
 	});
 }
 
-module.exports = function (keyword, cb) {
-	get(keyword, 3, function (err, data) {
-		if (err) {
-			cb(err);
-			return;
-		}
-
-		cb(null, data.map(function (el) {
+module.exports = function (keyword) {
+	return get(keyword, 3).then(function (data) {
+		return data.map(function (el) {
 			return {
 				name: el.key[1],
 				description: el.key[2]
 			};
-		}));
+		});
 	});
 };
 
-module.exports.names = function (keyword, cb) {
-	get(keyword, 2, function (err, data) {
-		if (err) {
-			cb(err);
-			return;
-		}
-
-		cb(null, data.map(function (el) {
+module.exports.names = function (keyword) {
+	return get(keyword, 2).then(function (data) {
+		return data.map(function (el) {
 			return el.key[1];
-		}));
+		});
 	});
 };
 
-module.exports.count = function (keyword, cb) {
-	get(keyword, 1, function (err, data) {
-		if (err) {
-			cb(err);
-			return;
-		}
-
-		cb(null, data[0] ? data[0].value : 0);
+module.exports.count = function (keyword) {
+	return get(keyword, 1).then(function (data) {
+		return data[0] ? data[0].value : 0;
 	});
 };
--- test.js
+++ test.js
@@ -3,10 +3,9 @@ var test = require('ava');
 var npmKeyword = require('./');
 
 test('npmKeyword()', function (t) {
-	t.plan(4);
+	t.plan(3);
 
-	npmKeyword('gulpplugin', function (err, packages) {
-		t.assert(!err, err);
+	npmKeyword('gulpplugin').then(function (packages) {
 		t.assert(packages.length > 0);
 		t.assert(packages[0].name.length > 0);
 		t.assert(packages[0].description.length > 0);
@@ -14,26 +13,23 @@ test('npmKeyword()', function (t) {
 });
 
 test('npmKeyword.names()', function (t) {
-	t.plan(3);
+	t.plan(2);
 
-	npmKeyword.names('gulpplugin', function (err, packageNames) {
-		t.assert(!err, err);
+	npmKeyword.names('gulpplugin').then(function (packageNames) {
 		t.assert(typeof packageNames[0] === 'string');
 		t.assert(packageNames[0].length > 0);
 	});
 });
 
 test('npmKeyword.count()', function (t) {
-	t.plan(5);
+	t.plan(3);
 
-	npmKeyword.count('gulpplugin', function (err, count) {
-		t.assert(!err, err);
+	npmKeyword.count('gulpplugin').then(function (count) {
 		t.assert(typeof count === 'number');
 		t.assert(count > 0);
 	});
 
-	npmKeyword.count('äąâ', function (err, count) {
-		t.assert(!err, err);
+	npmKeyword.count('äąâ').then(function (count) {
 		t.assert(count === 0);
 	});
 });
